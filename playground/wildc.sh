80c2776d15c3166072abeb79aaac8bed8d0e2c3a
Sabyasachi Mitra <sabyasachihyd2003@gmail.com>
2024-12-01 19:12:26 +0530

    linux 011224

---
+++ playground/wildc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Desc: Running Script or Command as Another User in Linux
+# We will learn different ways to run a script/command as 
+# another user in Linux using su or sudo commands
+#
+# Author: Sabyasachi Mitra
+# Date: 11/24/2024
